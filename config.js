02725304853665eca1a0bb2093570bbd752182f9
Stanislav Kalashnik <sk@infomir.eu>
2016-01-12 19:07:07 +0200

    add config file

---
+++ config.js
@@ -0,0 +1,33 @@
+/**
+ * Configuration for pack gulp task.
+ *
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// public
+module.exports = {
+    // turn on/off compilation
+    active: true,
+
+    // set of named configs for corresponding gulp tasks
+    // each profile inherits all options from the "default" profile
+    profiles: {
+        // config to be extended by other profiles
+        default: {
+
+        },
+
+        // config for pack:build:develop task
+        develop: {
+
+        },
+
+        // config for pack:build:release task
+        release: {
+
+        }
+    }
+};
